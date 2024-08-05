707936c2eb86ad7808385608d0f51c5aa6124ec2
Benjamin Gallarreta <benjamingallarreta@gmail.com>
2024-08-05 11:31:27 -0300

    Actualización de app Usuario

--- Usuario/models.py
+++ Usuario/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-#SE EXTIENDE DE USUARIO QUE VIENE POR DEFECTO DE DJANGO
 class Usuario(AbstractUser):
-    domicilio = models.CharField(max_length=250, null=True, blank=True)
-    nacionalidad = models.CharField(max_length=80, null=True, blank=True)
-    nroTelefono = models.CharField(max_length=80, null=True, blank=True)
-    correoElectronicoSecundario = models.CharField(max_length=250, null=True, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    fotoPerfil = models.ImageField(blank=True, upload_to='PONER NOMBRE DE LA CARPETA AQUI')
+    foto_perfil = models.ImageField(upload_to='user/static/')
+    nombre_completo = models.CharField(max_length=100, blank=False)
+    email = models.CharField(max_length=100, blank=False)
+    telefono = models.CharField(max_length=50, blank=True, null=True)
+    nacionalidad =  models.CharField(max_length=50, blank=False)
+    provincia = models.CharField(max_length=50, blank=False)
+    codigo_postal = models.CharField(max_length=5, blank=False)
+    ciudad = models.CharField(max_length=50, blank=False)
+    direccion = models.CharField(max_length=50, blank=False) 
 
     def _str_(self):
         return self.username
-    
-class BilleteraVirtual(models.Model):
-    monto = models.IntegerField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=None)
--- Usuario/views.py
+++ Usuario/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+from user.forms.profile_forms import ProfileForm
 
-# Create your views here.
+class ProfileView(View):
+    def get(self, request):
+        form = ProfileForm()
+        return render(request, 'myapp/profile.html', {'form': form})
+
+    def post(self, request):
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid() and request.FILES.get('profile_pic'):
+            profile_pic = request.FILES['profile_pic']
+            fs = FileSystemStorage()
+            filename = fs.save(profile_pic.name, profile_pic)
+            uploaded_file_url = fs.url(filename)
+            # Aquí puedes actualizar el modelo del usuario con la nueva ruta de la imagen
+            # user.profile_pic = uploaded_file_url
+            # user.save()
+            return redirect('profile_view')  # Redirige a la misma vista para actualizar la imagen
+
+        return render(request, 'myapp/profile.html', {'form': form})
